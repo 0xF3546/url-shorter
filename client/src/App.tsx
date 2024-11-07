3b349cc02ef7c487e2ffe856cad0a87739cb9742
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2024-11-07 14:39:34 +0100

    simple implimentation of url redirection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import { useEffect } from 'react';
 import './App.css';
 import {
   Routes,
   Route,
+  useParams,
 } from "react-router-dom";
 
 export const server = "http://localhost:3001";
@@ -10,13 +12,21 @@ const App: React.FC = () => {
   return (
     <div>
       <Routes>
-        <Route path='/*' element={< Main />} />
+        <Route path='/:identifier' element={< Main />} />
       </Routes>
     </div>
   );
 }
 
 const Main = () => {
+  const { identifier } = useParams();
+  useEffect(() => {
+    async () => {
+      const response = await fetch(`${server}/url/${identifier}`);
+      const data = await response.json();
+      window.location.href = data;
+    }
+  })
   return (
     <>
     </>
