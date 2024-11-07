f5d1f6229298eb5a23eaa326cc56732bb227acc2
0xF3546 <99101632+0xF3546@users.noreply.github.com>
2024-11-07 14:36:44 +0100

    added url loading

--- server/src/Controller/HomeController.ts
+++ server/src/Controller/HomeController.ts
@@ -1,15 +1,22 @@
 import { Router } from "express";
-import { emailService } from "../utils/EmailService.js";
+import { database } from "../index.js";
+
+const getUrl = async (identifier: string) => {
+    const result: any = await database.query("SELECT url FROM urls WHERE identifier = ?", identifier);
+    if (result.length === 0) {
+        return null;
+    }
+    return result[0].url;
+};
 
 const router: Router = Router();
 
-router.post('/sendContact', async (req, res) => {
-    const username = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message;
-    await emailService.sendAsync("management@voidroleplay.de", "[System] Kontaktformular", `Hallo Team!<br><br>${username} hat eine Nachricht über das Kontaktformular gesendet.<br>Message: ${message}<br>Email: ${email}`);
-    await emailService.sendAsync(email, "Kontaktformular eingegangne", `Hallo ${username}!<br><br>Wir haben deine Nachricht erhalten und kümmern uns so schnell wie möglich drum.`);
-    return res.json();
+router.get('/url/:identifier', async (req, res) => {
+    const url = await getUrl(req.params.identifier);
+    if (url) {
+       return res.json({url});
+    }
+    return res.status(404).json({message: 'URL not found'});
 });
 
 export const HomeRouter: Router = router;
